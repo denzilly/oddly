2593be5a098adb136417e5cde0a7cfd375453dc2
denzilly <btimmer313@gmail.com>
2021-06-13 23:02:51 +0200

    sqlite

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pystore
 import os
 import pandas
 import time
+import datetime
+
+import sqlite3
+
 
 from xpaths import *
 
@@ -9,33 +13,92 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.options import Options
 
 
-driver = webdriver.Firefox()
+### connect to the DB
+try:
+    connection = sqlite3.connect(os.getcwd()+'\data\odds_DB.db')
+    cursor = connection.cursor()
+    now = datetime.datetime.now()
+    sqlite_select_Query = "select sqlite_version();"
+
+
+except sqlite3.Error as error:
+    print("Error while connecting to sqlite", error)
+
+
+
+options=Options()
+options.headless = True
+
+
+driver = webdriver.Firefox(options=options)
 driver.get("https://www.paddypower.com/football/uefa-euro-2020?tab=outrights")
 
 driver.execute_script("window.scrollTo(0, 500)")
-
-WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, p_xpaths("none")["showall"]))).click()
-time.sleep(1)
+time.sleep(3)
 WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, p_xpaths("none")["cookies"]))).click()
 
 
-for x in range(1,25):
-    print(driver.find_element_by_xpath(p_xpaths(str(x))["country"]).get_attribute("innerHTML"))
-    print(driver.find_element_by_xpath(p_xpaths(str(x))["odds"]).get_attribute("innerHTML"))
+while True:
+    
 
+    
+    time.sleep(1)
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, p_xpaths("none")["showall"]))).click()
+    
+    
+
+
+    nr = {time : datetime.datetime.now()}
+    for x in range(1,25):
+        country = driver.find_element_by_xpath(p_xpaths(str(x))["country"]).get_attribute("innerHTML").upper().replace(" ","_")
+        try:
+            odds = driver.find_element_by_xpath(p_xpaths(str(x))["odds"]).get_attribute("innerHTML")
+        except:
+            odds = 0
+        nr[country] = odds
+
+
+    #push the data to the DB
+
+    cursor.execute("""SELECT *
+                            FROM ODDS_DATA
+                            ORDER BY id DESC
+                            LIMIT 1"""
+                            )
+    try:
+        new_id = cursor.fetchall()[0][0] + 1
+    except IndexError:
+        new_id = 1
+
+    now = datetime.datetime.now()
+    insert = f""" INSERT INTO ODDS_DATA(id,time,FRANCE,GERMANY,ENGLAND,BELGIUM,SPAIN,ITALY,PORTUGAL,
+                    NETHERLANDS,CROATIA,SWITZERLAND,DENMARK,UKRAINE,SWEDEN,AUSTRIA,POLAND,TURKEY,CZECH_REPUBLIC,
+                    WALES,SCOTLAND,RUSSIA,FINLAND,NORTH_MACEDONIA,HUNGARY,SLOVAKIA) 
+                    VALUES({new_id},'{str(now)}','{nr["FRANCE"]}','{nr["GERMANY"]}','{nr["ENGLAND"]}','{nr["BELGIUM"]}',
+                    '{nr["SPAIN"]}','{nr["ITALY"]}','{nr["PORTUGAL"]}','{nr["NETHERLANDS"]}','{nr["CROATIA"]}','{nr["SWITZERLAND"]}',
+                    '{nr["DENMARK"]}','{nr["UKRAINE"]}','{nr["SWEDEN"]}','{nr["AUSTRIA"]}','{nr["POLAND"]}','{nr["TURKEY"]}','{nr["CZECH_REPUBLIC"]}',
+                    '{nr["WALES"]}','{nr["SCOTLAND"]}','{nr["RUSSIA"]}','{nr["FINLAND"]}','{nr["NORTH_MACEDONIA"]}','{nr["HUNGARY"]}','{nr["SLOVAKIA"]}'
+    )"""
+
+
+    cursor.execute(insert)
+    connection.commit()
+    print("wrote data to DB")
+    
+    driver.refresh()
+
+
+
+cursor.close()
 
 
 time.sleep(10)
 driver.close()
 
 
-db_path = os.getcwd() + "\\data"
-pystore.set_path(db_path)
-
-store = pystore.store('odds_data')
-pystore.list_stores()
 
 
 time.sleep(10)
--- test.py
+++ test.py
@@ -1,22 +1,42 @@
-import pystore
+import sqlite3
 import os
+import datetime
+import time
 
-import pandas as pd
+try:
+    connection = sqlite3.connect(os.getcwd()+'\data\odds_DB.db')
+    cursor = connection.cursor()
+    now = datetime.datetime.now()
+    sqlite_select_Query = "select sqlite_version();"
+      
+
+    cursor.execute("""SELECT *
+                        FROM ODDS_DATA
+                        ORDER BY id DESC
+                        LIMIT 1"""
+                        )
+    new_id = cursor.fetchall()[0][0] + 1
+    print(new_id)
 
 
-cars = {'Brand': ['Honda Civic','Toyota Corolla','Ford Focus','Audi A4'],
-        'Price': [22000,25000,27000,35000]
-        }
+    # insert = f""" INSERT INTO ODDS_DATA(id,time,FRANCE,GERMANY,ENGLAND,BELGIUM,SPAIN,ITALY,PORTUGAL,NETHERLANDS,CROATIA,SWITZERLAND,DENMARK,UKRAINE,SWEDEN,AUSTRIA,POLAND,TURKEY,CZECH_REPUBLIC,WALES,SCOTLAND,RUSSIA,FINLAND,NORTH_MACEDONIA,HUNGARY,SLOVAKIA) 
+    # VALUES(4,'{str(now)}',2,1,3,4,2,3,54,2,3,5,2,3,5,43,5,3,43,3,2,3,4,5,3,2)"""
+    
+    # cursor.execute(insert)
+    # connection.commit()
 
-df = pd.DataFrame(cars, columns = ['Brand', 'Price'])
+    cursor.close()
+
+except sqlite3.Error as error:
+    print("Error while connecting to sqlite", error)
 
 
+finally:
+    if connection:
+        
+        connection.close()
+        print("The SQLite connection is closed")
 
 
-db_path = os.getcwd() + "\\data"
-pystore.set_path(db_path)
+# create table
 
-store = pystore.store('test_data')
-collection = store.collection("coll1")
-
-store.list_collections()
